20d61d29e9808fa9fe5c7b29028bd6154ca3dc12
Lahl, Dominique <47154137+Tesselay@users.noreply.github.com>
2025-09-08 00:33:41 +0200

    Make content base path relative

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,8 +2,10 @@ import { glob } from "astro/loaders";
 import { defineCollection } from "astro:content";
 import { BlogPostSchema } from "./schema";
 
+const blogBase = new URL("./blog", import.meta.url);
+
 const postsCollection = defineCollection({
-  loader: glob({ pattern: "**/[^_]*.md", base: "./src/content/blog" }),
+  loader: glob({ pattern: "**/[^_]*.md", base: blogBase }),
   schema: BlogPostSchema,
 });
 
