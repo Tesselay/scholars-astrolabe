689482c36740264b961239726cfa22b4ec63b50d
Lahl, Dominique <47154137+Tesselay@users.noreply.github.com>
2025-09-09 17:06:32 +0200

    Reverse to URL base
    Maybe it'll work now in both environments

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,8 +2,10 @@ import { glob } from "astro/loaders";
 import { defineCollection } from "astro:content";
 import { BlogPostSchema } from "./content/schema.ts";
 
+const blogBase = new URL("./content/blog/", import.meta.url);
+
 const postsCollection = defineCollection({
-  loader: glob({ pattern: "**/[^_]*.md", base: "./content/blog" }),
+  loader: glob({ pattern: "**/[^_]*.md", base: blogBase }),
   schema: BlogPostSchema,
 });
 
