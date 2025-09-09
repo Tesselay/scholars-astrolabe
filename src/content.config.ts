22c7a34461325264b29df27ff1e4947a4174c298
Lahl, Dominique <47154137+Tesselay@users.noreply.github.com>
2025-09-09 17:16:33 +0200

    Hardcode glob path to pattern

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,10 +2,8 @@ import { glob } from "astro/loaders";
 import { defineCollection } from "astro:content";
 import { BlogPostSchema } from "./content/schema.ts";
 
-const blogBase = new URL("./content/blog/", import.meta.url);
-
 const postsCollection = defineCollection({
-  loader: glob({ pattern: "**/[^_]*.md", base: blogBase }),
+  loader: glob({ pattern: "src/content/blog/**/[^_]*.md" }),
   schema: BlogPostSchema,
 });
 
