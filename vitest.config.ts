2368669e96e90fb339e1f49ec789add6ab6df404
Lahl, Dominique <47154137+Tesselay@users.noreply.github.com>
2025-09-10 04:37:57 +0200

    Merge astro with vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,22 @@
-/// <reference types="vitest" />
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
+import { getViteConfig } from "astro/config";
+import { mergeConfig, type InlineConfig } from "vite";
 
-export default defineConfig({
-  test: {
-    environment: "node",
-  },
+export default defineConfig(async () => {
+  let base = getViteConfig({ mode: "test" });
+
+  if (typeof base === "function") {
+    // @ts-expect-error irrelevant type mismatch
+    base = await base({ command: "serve", mode: "test" });
+  }
+
+  const astroVite = base as InlineConfig;
+
+  const vitestOnly: InlineConfig = {
+    test: {
+      environment: "node",
+    },
+  };
+
+  return mergeConfig(astroVite, vitestOnly);
 });
