a5777d635d7edb928d16f42f5699b2c9811e2c4b
Lahl, Dominique <47154137+Tesselay@users.noreply.github.com>
2026-01-27 13:07:17 +0100

    Simplify element building for buttons

--- src/components/Button/button.d.ts
+++ src/components/Button/button.d.ts
@@ -3,5 +3,5 @@ import type { HTMLAttributes } from "astro/types";
 export interface ButtonBase extends Pick<HTMLAttributes<"button">, "disabled"> {
   variant?: "primary" | "ghost" | "icon" | "danger";
   size?: "xs" | "s" | "m" | "l" | "xl";
-  color?: string;
+  color?: string | undefined | null;
 }
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,9 +1,9 @@
 import type { ButtonBase } from "@/components/Button/button.d.ts";
 
-export function getButtonClasses(props: ButtonBase) {
-  return ["btn", props.variant && `btn--${props.variant}`, props.size && `btn--${props.size}`];
+export function getButtonClasses(variant?: ButtonBase["variant"], size?: ButtonBase["size"]) {
+  return ["btn", variant && `btn--${variant}`, size && `btn--${size}`];
 }
 
-export function getButtonStyles(props: ButtonBase) {
-  return [props.color && `--btn-accent: ${props.color}`];
+export function getButtonStyles(color?: ButtonBase["color"]) {
+  return color ? { "--btn-accent": color } : {};
 }
