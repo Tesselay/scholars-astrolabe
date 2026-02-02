5eda7e38648ec432014e74ac7095955030d89e8d
Lahl, Dominique <47154137+Tesselay@users.noreply.github.com>
2026-02-02 20:14:07 +0100

    Delete attributes.d.ts
    Missed it on refactor.

--- src/types/attributes.d.ts
+++
@@ -1,9 +0,0 @@
-import type { JSX } from "astro/jsx-runtime";
-import type { HTMLAttributes } from "astro/types";
-
-export interface PatchOptions<Type extends keyof JSX.DefinedIntrinsicElements> {
-  classList?: Array<string | false | null | undefined>;
-  styleList?: Array<string | false | null | undefined>;
-  decorative?: boolean;
-  attributes?: HTMLAttributes<Type>;
-}
