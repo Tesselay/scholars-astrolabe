c5595d675d927d8a024531357cd7a4d6aa11fe79
Lahl, Dominique <47154137+Tesselay@users.noreply.github.com>
2026-02-07 18:53:31 +0100

    Add exclusions for protocol pages

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,10 +2,13 @@ import { defineMiddleware, sequence } from "astro/middleware";
 import { middleware, requestHasLocale, redirectToDefaultLocale } from "astro:i18n";
 import { TEST_PAGE } from "astro:env/server";
 
+const localeExclusive = ["/sitemap", "/robots", "/.well-known", "/humans"];
 export const localeValidation = defineMiddleware(async (context, next) => {
   const response = await next();
 
   if (context.url.pathname.startsWith("/test") && TEST_PAGE) {
+    return response;
+  } else if (localeExclusive.some((p) => context.url.pathname.startsWith(p))) {
     return response;
   } else if (!requestHasLocale(context)) {
     return redirectToDefaultLocale(context, 307)!;
