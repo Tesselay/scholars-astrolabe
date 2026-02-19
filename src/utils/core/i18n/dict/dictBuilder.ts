facd9c140dd203519cea25a736211284d2b6cb2c
Lahl, Dominique <47154137+Tesselay@users.noreply.github.com>
2026-02-19 01:46:24 +0100

    Let zod infer dictionary type from schema
    Instead of duplicating source of truth

--- src/utils/core/i18n/dict/dictBuilder.ts
+++ src/utils/core/i18n/dict/dictBuilder.ts
@@ -1,14 +1,15 @@
 import modelUI from "@/utils/core/i18n/dict/dictionaries/en/ui.json";
 import modelMeta from "@/utils/core/i18n/dict/dictionaries/en/meta.json";
-import { buildStrictSchema, type StringLeaves } from "./schemaBuilder.ts";
+import { buildStrictSchema } from "./schemaBuilder.ts";
 import { GenericDictLoader } from "./genericDictLoader.ts";
 import { metaDictModules, uiDictModules } from "@/utils/adapter/vite/dicts.ts";
+import { z } from "zod";
 
 export const UiSchema = buildStrictSchema(modelUI);
 export const MetaSchema = buildStrictSchema(modelMeta);
 
-export type Ui = StringLeaves<typeof modelUI>;
-export type Meta = StringLeaves<typeof modelMeta>;
+export type Ui = z.infer<typeof UiSchema>;
+export type Meta = z.infer<typeof MetaSchema>;
 
 export const uiLoader = new GenericDictLoader<Ui>("ui", UiSchema, uiDictModules);
 export const metaLoader = new GenericDictLoader<Meta>("meta", MetaSchema, metaDictModules);
