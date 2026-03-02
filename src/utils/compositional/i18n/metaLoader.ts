e520215543cff2a1efcb104a2d20a6493680b7ed
Lahl, Dominique <47154137+Tesselay@users.noreply.github.com>
2026-03-02 21:14:30 +0100

    Move dictionaries to content layer
    Clearer source instead of having it nested deeply within utils

--- src/utils/compositional/i18n/metaLoader.ts
+++ src/utils/compositional/i18n/metaLoader.ts
@@ -1,7 +1,7 @@
 import { buildStrictSchema } from "@/utils/core/i18n/dict/schemaBuilder.ts";
 import { GenericDictLoader } from "@/utils/core/i18n/dict/genericDictLoader.ts";
 import { metaDictModules } from "@/utils/adapter/vite/dicts.ts";
-import modelMeta from "@/utils/core/i18n/dict/dictionaries/en/meta.json";
+import modelMeta from "@/content/dictionaries/en/meta.json";
 
 const MetaSchema = buildStrictSchema(modelMeta);
 export const MetaLoader = new GenericDictLoader("meta", MetaSchema, metaDictModules);
--- src/utils/compositional/i18n/uiLoader.ts
+++ src/utils/compositional/i18n/uiLoader.ts
@@ -1,7 +1,7 @@
 import { GenericDictLoader } from "@/utils/core/i18n/dict/genericDictLoader.ts";
 import { uiDictModules } from "@/utils/adapter/vite/dicts.ts";
 import { buildStrictSchema } from "@/utils/core/i18n/dict/schemaBuilder.ts";
-import modelUI from "@/utils/core/i18n/dict/dictionaries/en/ui.json";
+import modelUI from "@/content/dictionaries/en/ui.json";
 
 const UiSchema = buildStrictSchema(modelUI);
 export const UiLoader = new GenericDictLoader("ui", UiSchema, uiDictModules);
