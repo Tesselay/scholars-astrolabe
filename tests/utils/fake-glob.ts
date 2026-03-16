05faa0f1c9fcbc9c4f0becfd93475a1b2133fdfa
Lahl, Dominique <47154137+Tesselay@users.noreply.github.com>
2026-03-16 20:59:33 +0100

    Fix missing async on test glob

--- tests/utils/fake-glob.ts
+++ tests/utils/fake-glob.ts
@@ -1,6 +1,6 @@
-export function fakeGlob<Type>(en: Type, de: Type, filename: string): Record<string, Type> {
-  return {
+export function fakeGlob<Type>(en: Type, de: Type, filename: string): Promise<Record<string, Type>> {
+  return Promise.resolve({
     [`/virtual/i18n/dictionaries/en/${filename}.json`]: en,
     [`/virtual/i18n/dictionaries/de/${filename}.json`]: de
-  };
+  });
 }
